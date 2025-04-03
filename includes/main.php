88362048098a7856cdd0210133e62e034974727d
matteo-cardellini <cardellini.matteo@icloud.com>
2025-04-03 08:02:31 +0200

    Update version for main

--- includes/main.php
+++ includes/main.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BetLAM - Gratis Wetten & Spaß</title>
     <style>
+        /*styling*/  
         body {
             font-family: Arial, sans-serif;
             margin: 0;
